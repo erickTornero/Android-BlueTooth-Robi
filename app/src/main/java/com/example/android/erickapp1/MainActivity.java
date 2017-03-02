a581d27a82c1a069b147cafff71c9f61bc40a7d5
erickTornero <erickdeivy01@gmail.com>
2017-03-01 20:05:44 -0500

    Add files via upload

--- app/src/main/java/com/example/android/erickapp1/MainActivity.java
+++ app/src/main/java/com/example/android/erickapp1/MainActivity.java
@@ -1,20 +1,31 @@
 package com.example.android.erickapp1;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.view.View;
 import android.bluetooth.BluetoothDevice;
+import android.util.Log;
+
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "tag";
     TextView textMessage;
     Button btnTestBT;
     BluetoothAdapter bluetAdapt;
-    ArrayAdapter <BluetoothDevice> arraySincrBT;
+    ArrayAdapter <String> arrayAdapter;
+    ListView listDevicesBT;
+    String []lista = {"itemA","itemB","itemC","itemD"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,14 +33,19 @@ public class MainActivity extends AppCompatActivity {
         //This is the code write for the programmer
         textMessage = (TextView) findViewById(R.id.textMessage);
         btnTestBT = (Button) findViewById(R.id.btnTestBt);
-    }
 
+        listDevicesBT = (ListView) findViewById(R.id.ListBtDevices);
+
+        arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,lista);
+        listDevicesBT.setAdapter(arrayAdapter);
+    }
 
     @Override
     protected void onPause() {
         super.onPause();
     }
 
+    //Metodo: Verifica si esta prendido bluetooth, en caso que no: intenta prenderlo
     public void startBluetooth(View v)
     {
         bluetAdapt = BluetoothAdapter.getDefaultAdapter();
@@ -49,9 +65,36 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+    //Lectura de dispositivos sincronizados mediante Bluetooth
     public void  bluetoothSinc(View v)
+    {
+        Set<BluetoothDevice> pairedDevices = bluetAdapt.getBondedDevices();
+        if(pairedDevices.size()>0)
+        {
+            //OBTIENE TODOS LOS DISPOSITIVOS SINCRONIZADOS
+            for(BluetoothDevice device:pairedDevices)
+            {
+                //arrayAdapter.add(device.getName().toString()+""/*"\n"+device.getAddress()*/);
+                /*Log.v(TAG, "PairedDevices: " + device.getName() + "  "
+                        + device.getAddress());*/
+                textMessage.setText(textMessage.getText()+"\n"+device.getName());
+            }
+        }
+    }
+
+    public void scanDevicesBt(View v)
     {
 
     }
-
+   /* private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if(BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                arrayAdapter.add(device.getName() + "\n" + device.getAddress());
+            }
+        }
+    };
+    IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);*/
 }
